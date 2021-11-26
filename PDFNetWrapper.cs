fe6a1bbb5c3bb37b836921d16c9f257a33b6c883
CorreyL <correylim@gmail.com>
2021-11-26 10:48:23 -0800

    Add wrapper file for PDFNet

---
+++ PDFNetWrapper.cs
@@ -0,0 +1,10 @@
+﻿using pdftron;
+
+namespace azure_hsm_signing
+{
+  public class PDFNetWrapper {
+    public PDFNetWrapper(string licenseKey) {
+      PDFNet.Initialize(licenseKey);
+    }
+  }
+}
