06e6da364b6184b85456984972347a7098ea1069
CorreyL <correylim@gmail.com>
2021-11-26 07:14:36 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace azure_hsm_signing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
