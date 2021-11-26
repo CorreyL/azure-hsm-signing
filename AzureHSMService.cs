ff1e15990ab04d614e4c995f353fa9df56ee0b67
CorreyL <correylim@gmail.com>
2021-11-26 08:30:52 -0800

    Change line endings and indentation
    - Use 2 spaces for each indent
    - Make line endings unix compatible (CRLF -> LF)

--- AzureHSMService.cs
+++ AzureHSMService.cs
@@ -6,34 +6,34 @@ using Azure.Security.KeyVault.Keys;
 using Azure.Security.KeyVault.Keys.Cryptography;
 
 namespace azure_hsm_signing {
-	public class AzureHSMService
-	{
-		private string debugCategory = "AzureHSMService";
-		CryptographyClient rsaCryptoClient;
-		public AzureHSMService()
-		{
-			// The URL that the KeyVault is hosted at on Azure
-			const string hsmUrl = "https://pdftron-hsm-digsigs.vault.azure.net/";
-			// The name of the non-exportable Certificate under the KeyVault named pdftron-hsm-digsigs
-			const string certificateName = "hsm-test";
+  public class AzureHSMService
+  {
+    private string debugCategory = "AzureHSMService";
+    CryptographyClient rsaCryptoClient;
+    public AzureHSMService()
+    {
+      // The URL that the KeyVault is hosted at on Azure
+      const string hsmUrl = "https://pdftron-hsm-digsigs.vault.azure.net/";
+      // The name of the non-exportable Certificate under the KeyVault named pdftron-hsm-digsigs
+      const string certificateName = "hsm-test";
 
-			KeyClient keyClient = new KeyClient(new Uri(hsmUrl), new VisualStudioCredential());
-			KeyVaultKey rsaKey = keyClient.GetKey(certificateName);
-			Debug.WriteLine($"Key is returned with name {rsaKey.Name} and type {rsaKey.KeyType}", debugCategory);
+      KeyClient keyClient = new KeyClient(new Uri(hsmUrl), new VisualStudioCredential());
+      KeyVaultKey rsaKey = keyClient.GetKey(certificateName);
+      Debug.WriteLine($"Key is returned with name {rsaKey.Name} and type {rsaKey.KeyType}", debugCategory);
 
-			rsaCryptoClient = new CryptographyClient(rsaKey.Id, new DefaultAzureCredential());
-		}
+      rsaCryptoClient = new CryptographyClient(rsaKey.Id, new DefaultAzureCredential());
+    }
 
-		public byte[] Sign(byte[] digest, SignatureAlgorithm? signatureAlgorithm = null)
-		{
-			// Since the default value in the function signature needs to be a compile-time constant, this is a workaround
-			SignatureAlgorithm algorithm = signatureAlgorithm ?? SignatureAlgorithm.RS256;
-			SignResult rsaSignResult = rsaCryptoClient.Sign(algorithm, digest);
-			Debug.WriteLine(
-				$"Signed digest using the algorithm {rsaSignResult.Algorithm}, with key {rsaSignResult.KeyId}. The resulting signature is {Convert.ToBase64String(rsaSignResult.Signature)}",
-				debugCategory
-			);
-			return rsaSignResult.Signature;
-		}
-	}
+    public byte[] Sign(byte[] digest, SignatureAlgorithm? signatureAlgorithm = null)
+    {
+      // Since the default value in the function signature needs to be a compile-time constant, this is a workaround
+      SignatureAlgorithm algorithm = signatureAlgorithm ?? SignatureAlgorithm.RS256;
+      SignResult rsaSignResult = rsaCryptoClient.Sign(algorithm, digest);
+      Debug.WriteLine(
+        $"Signed digest using the algorithm {rsaSignResult.Algorithm}, with key {rsaSignResult.KeyId}. The resulting signature is {Convert.ToBase64String(rsaSignResult.Signature)}",
+        debugCategory
+      );
+      return rsaSignResult.Signature;
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using System.Security.Cryptography;
 
 namespace azure_hsm_signing
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            AzureHSMService azureHSMService = new AzureHSMService();
-            byte[] messageToSign = System.Text.Encoding.ASCII.GetBytes("Hello World");
-            byte[] hashValue = SHA256.Create().ComputeHash(messageToSign);
-            azureHSMService.Sign(hashValue);
-        }
+      AzureHSMService azureHSMService = new AzureHSMService();
+      byte[] messageToSign = System.Text.Encoding.ASCII.GetBytes("Hello World");
+      byte[] hashValue = SHA256.Create().ComputeHash(messageToSign);
+      azureHSMService.Sign(hashValue);
     }
+  }
 }
