1b82062cf57f2fae56577bf46e29b885268d5bc3
CorreyL <correylim@gmail.com>
2021-11-26 10:49:01 -0800

    Add method to prep PDF for custom signing
    Adds a dictionary to the signature field where the PKCS#7 message with
    the Signed Hash from the Azure KeyVault HSM will be placed into

--- PDFNetWrapper.cs
+++ PDFNetWrapper.cs
@@ -1,10 +1,44 @@
-﻿using pdftron;
+﻿using System;
+
+using pdftron;
+using pdftron.PDF;
+using pdftron.PDF.Annots;
+using pdftron.SDF;
 
 namespace azure_hsm_signing
 {
   public class PDFNetWrapper {
     public PDFNetWrapper(string licenseKey) {
       PDFNet.Initialize(licenseKey);
+    }
+    public PDFDoc PreparePdfForCustomSigning(PDFDoc doc, string signatureFieldName, uint sizeOfContents = 7500)
+    {
+      Page page1 = doc.GetPage(1);
+      Field found_approval_field = doc.GetField(signatureFieldName);
+      bool isLockedByDigitalSignature = found_approval_field != null && found_approval_field.IsLockedByDigitalSignature();
+      if (isLockedByDigitalSignature)
+      {
+        throw new Exception($"The field {signatureFieldName} is locked by a Digital Signature, and thus cannot be Digitally Signed again");
+      }
+
+      DigitalSignatureField certification_sig_field = doc.CreateDigitalSignatureField(signatureFieldName);
+      SignatureWidget widgetAnnot = SignatureWidget.Create(doc, new Rect(), certification_sig_field);
+
+      page1.AnnotPushBack(widgetAnnot);
+      certification_sig_field.SetDocumentPermissions(DigitalSignatureField.DocumentPermissions.e_no_changes_allowed);
+
+      // Prepare the signature and signature handler for signing.
+      certification_sig_field.CreateSigDictForCustomSigning(
+          "Adobe.PPKLite",
+          // This chosen enum assumes you wish to use a PADES compliant signing mode
+          // Please see this documentation for all possible options
+          // https://www.pdftron.com/api/PDFTronSDK/dotnet/pdftron.PDF.DigitalSignatureField.SubFilterType.html
+          DigitalSignatureField.SubFilterType.e_ETSI_CAdES_detached,
+          sizeOfContents
+      );
+
+      byte[] newDoc = doc.Save(SDFDoc.SaveOptions.e_incremental);
+      return new PDFDoc(newDoc, newDoc.Length);
     }
   }
 }
