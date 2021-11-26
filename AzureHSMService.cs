b1b4da40f5cb0f3c267f64de58a634fdfcc9691e
CorreyL <correylim@gmail.com>
2021-11-26 07:43:26 -0800

    Instantiate CryptographyClient in AzureHSMService
    Ensures the class can then create signing and verification methods

--- AzureHSMService.cs
+++ AzureHSMService.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
+
+using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
 
 namespace azure_hsm_signing {
 	public class AzureHSMService
 	{
+		CryptographyClient rsaCryptoClient;
 		public AzureHSMService()
 		{
-			Console.WriteLine("Hello World");
+			// The URL that the KeyVault is hosted at on Azure
+			const string hsmUrl = "https://pdftron-hsm-digsigs.vault.azure.net/";
+			// The name of the non-exportable Certificate under the KeyVault named pdftron-hsm-digsigs
+			const string certificateName = "hsm-test";
+
+			KeyClient keyClient = new KeyClient(new Uri(hsmUrl), new VisualStudioCredential());
+			KeyVaultKey rsaKey = keyClient.GetKey(certificateName);
+			Debug.WriteLine($"Key is returned with name {rsaKey.Name} and type {rsaKey.KeyType}");
+
+			rsaCryptoClient = new CryptographyClient(rsaKey.Id, new DefaultAzureCredential());
 		}
 	}
 }
